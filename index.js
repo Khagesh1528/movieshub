dae0f10a234b89ef09aa065b85a845c3826efe59
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-06-27 19:39:59 +0530

    Test App For Vercel Live App

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const port = 4000;
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views','./views')
+
+app.get('/',function(req,res){
+    return res.render('home')
+})
+
+app.listen(port,function(error){
+        if(error){
+            console.log('Error Come In Starting Server',error);
+        }
+            console.log('Server is running on port ::',port);
+});
+
+
+
